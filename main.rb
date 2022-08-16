986b4098b256d27c8c7604d0435290dc22546121
Allan <kipropsallan@gmail.com>
2022-08-16 12:10:19 +0300

    Code along

--- main.rb
+++ main.rb
@@ -2,28 +2,46 @@
 
 # 1.0 Printing Methods
 # TODO: puts
+puts "Hello World!"
 
 # TODO: print
+print "Something new"
 
 # TODO: pp
+pp [1,2,3,4]
+puts [1,2,3,4]
 
 # 2.0 Strings
 # TODO: CREATE A STRING
+name = "Allan"
 
 # TODO: CREATE A STRING WITH SINGLE QUOTES
+last_name ='Kiprop'
 
 # TODO: INTERPOLATE STRINGS
+full_name = "My name is : #{name} #{last_name}"
+puts full_name
 
 # TODO: String methods (upcase, downcase, capitalize)
+puts full_name.upcase
+puts full_name.downcase
+puts 'king maker'.capitalize
 
 # 3.0 Numbers
 # TODO: Integers
+age = 80
+height = 130
+weight = 64
 
 # TODO: Float
+weight = 69.6
+distance = 123.45
 
 # TODO: Number methods (floor, ceil, to_i, to_f)
-
-
+puts 444.5.floor
+puts 0.989.ceil
+puts 69.99.to_i
+puts 0.66.to_i
 # 4.0 Boolean
 # TODO: True / Truthy
 
