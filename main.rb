e84174daf7a128fe89b5ae3692ff80e9f1f95326
Ian Okumu <okumu.otsembo@gmail.com>
2022-08-16 06:36:21 +0300

    Setup complete

---
+++ main.rb
@@ -0,0 +1,55 @@
+# COMMENTS ARE IN HASHES
+
+# 1.0 Printing Methods
+# TODO: puts
+
+# TODO: print
+
+# TODO: pp
+
+# 2.0 Strings
+# TODO: CREATE A STRING
+
+# TODO: CREATE A STRING WITH SINGLE QUOTES
+
+# TODO: INTERPOLATE STRINGS
+
+# TODO: String methods (upcase, downcase, capitalize)
+
+# 3.0 Numbers
+# TODO: Integers
+
+# TODO: Float
+
+# TODO: Number methods (floor, ceil, to_i, to_f)
+
+
+# 4.0 Boolean
+# TODO: True / Truthy
+
+# TODO: False / Falsy
+
+# 5.0 Symbols
+# TODO: Create symbol
+
+
+# 6.0 Arrays
+# TODO: Create array natively
+
+# TODO: Create array using Array class
+
+# 7.0 Hashes
+# TODO: Create Hash
+
+# TODO: Create Hash using Hash class
+
+# 8.0 Methods
+# TODO: Method with enclosed params
+
+# TODO: Method with open params
+
+
+
+
+
+
